ad61aa09f6ad0c5add4843e9f0548927aa091852
MarkusMayer1 <markus.mayer1@stud.fh-campuswien.ac.at>
2024-10-08 15:43:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~>3.0"
     }
   }
+
+  backend "azurerm" {
+      resource_group_name  = "devOpsMain"
+      storage_account_name = "startupnextjstfstate"
+      container_name       = "startupnextjstfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
