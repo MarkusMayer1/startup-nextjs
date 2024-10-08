5ecb64168edcffd8a57660e2b59db71d914c8a5b
MarkusMayer1 <markus.mayer1@stud.fh-campuswien.ac.at>
2024-10-08 11:21:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,15 @@ variable "azure_subscription_id" {
   type = string
 }
 
+variable "azure_tenant_id" {
+  type = string
+}
+
 provider "azurerm" {
   features {}
 
   subscription_id = var.azure_subscription_id
+  tenant_id = var.azure_tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
